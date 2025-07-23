9d225e8d4592fd30d25f25e7db26fff13ed9e61e
Victor <victor.oliveira4942081@edu.pe.senac.br>
2025-07-23 10:45:04 -0300

    Versão 001

---
+++ Trabalho.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+
+public class Trabalho {
+
+    public static void main(String[] args) {
+        System.out.println("Teste do repositório GitHub: Sicoco Bala discipules");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hohohohoho, bem-vindo, sir. ");
+    }
+}
